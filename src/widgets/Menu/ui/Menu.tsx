abc67a5c1bb13d103db7307af3bf767ed50643b8
dknife <dknifeb@mail.ru>
2024-04-05 20:52:44 +0300

    fix: after code reviow

--- src/widgets/Menu/ui/Menu.tsx
+++ src/widgets/Menu/ui/Menu.tsx
@@ -4,22 +4,24 @@ import { Home, Heart, ListMusic, Settings } from 'lucide-react';
 
 export const Menu = () => {
     const MenuInfo = [
-        {title:'Discover',img:<Home size = {16}/>},
-        {title:'Liked',img:<Heart size={16}/>},
-        {title:'PlayList',img:<ListMusic size={16}/>}, 
-        {title:'Settings',img:<Settings size={16}/>}
-        
+        { id: 1, title: 'Discover', img: <Home size={16} />, link: '/' },
+        { id: 2, title: 'Liked', img: <Heart size={16} />, link: '/liked' },
+        { id: 3, title: 'PlayList', img: <ListMusic size={16} />, link: '/playlist' },
+        { id: 4, title: 'Settings', img: <Settings size={16} />, link: '/settings' }
+
     ]
     return (
-        <div className='bg-white max-h-17 flex justify-center '>
+        <div className='bg-white max-h-17 flex justify-center'>
             <div className='max-w-84 px-5'>
-                <ul className = 'grid grid-cols-4 gap-x-5 '>
+                <ul className='grid grid-cols-4 gap-x-5 '>
                     {
-                        MenuInfo.map((item,i)=>(
-                            <li className='flex items-center flex-col gap-y-1   px-4 py-2 hover:bg-sky-400 ' key={i}>
-                                  {item.img}
-                                <p className = 'text-xs leading-32'>{item.title}</p>
-                            </li>
+                        MenuInfo.map(item => (
+                            <Link to={item.link}>
+                                <li className='flex items-center flex-col gap-y-1 px-4 py-2 hover:bg-sky-400 rounded-md' key={item.id}>
+                                    {item.img}
+                                    <p className='text-xs leading-32'>{item.title}</p>
+                                </li>
+                            </Link>
                         ))
                     }
                 </ul>
