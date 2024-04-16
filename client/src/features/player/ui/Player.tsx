1b567bd1ca093dcfbb1711957e5619d0dfb26410
LassTwoKey <dimas.super29@gmail.com>
2024-04-16 19:44:30 +0300

    fix: change ui in app player

--- client/src/features/player/ui/Player.tsx
+++ client/src/features/player/ui/Player.tsx
@@ -6,6 +6,7 @@ import { RootState } from '@/app/providers/StoreProvider'
 import { setPlayerInfo } from '@/app/providers/StoreProvider/config/playerSlice'
 import { convertSecondsToTime } from '@/shared/lib/player'
 import { cn } from '@/shared/lib/utils'
+import { Button } from '@/shared/ui/button'
 import { Card } from '@/shared/ui/card'
 import { Popover, PopoverContent, PopoverTrigger } from '@/shared/ui/popover'
 import { Slider } from '@/shared/ui/slider'
@@ -123,97 +124,123 @@ export const Player = () => {
     }
 
     return (
-        <div className="container">
+        <>
             <input type="file" onChange={handleFileChange} />
-            <Card
-                className={`px-2 ${isOpenOrCloseAudioPlayer} transition-opacity ease-out duration-300`}
+            <div
+                className={`bg-card py-2 border-b-2 ${isOpenOrCloseAudioPlayer} transition-opacity ease-out duration-300`}
             >
-                <audio ref={audioRef} className="w-full">
-                    {audioUrl && <source src={audioUrl} type="audio/mpeg" />}
-                </audio>
-                <div className="flex justify-between items-center px-2 py-2 gap-2">
-                    <div className="flex items-center">
-                        <div className="w-20 overflow-hidden rounded-lg">
-                            <img
-                                className="w-full h-auto"
-                                src={
-                                    imgUrl ||
-                                    'https://img.freepik.com/premium-vector/error-404-not-found-glitch-effect_8024-4.jpg?w=1800'
-                                }
-                                alt="Album Cover"
-                            />
+                <div className="container">
+                    <audio ref={audioRef} className="w-full">
+                        {audioUrl && <source src={audioUrl} type="audio/mpeg" />}
+                    </audio>
+                    <div className="flex mb-1 justify-between items-center gap-2">
+                        <div className="flex items-center">
+                            <div className="w-10 h-10 overflow-hidden rounded-lg">
+                                <img
+                                    className="w-full h-auto"
+                                    src={
+                                        imgUrl ||
+                                        'https://img.freepik.com/premium-vector/error-404-not-found-glitch-effect_8024-4.jpg?w=1800'
+                                    }
+                                    alt="Album Cover"
+                                />
+                            </div>
+                            <div className="text ml-3 flex flex-col">
+                                <h3 className="text-sm text-primary-foreground font-medium">
+                                    {name || 'Unknown music name'}
+                                </h3>
+                                <p className="text-xs mr-8">{author || 'Unknown author'}</p>
+                            </div>
                         </div>
-                        <div className="text ml-3 flex flex-col gap-2">
-                            <h3 className="text-sm text-primary-foreground font-medium">
-                                {name || 'Unknown music name'}
-                            </h3>
-                            <p className="text-xs mr-8">{author || 'Unknown author'}</p>
-                        </div>
-                    </div>
-                    <div className="flex items-center gap-3">
-                        {isPlaying ? (
-                            <Pause size={20} onClick={togglePlay} />
-                        ) : (
-                            <Play size={20} onClick={togglePlay} />
-                        )}
-                        <div className="slider">
-                            <Popover>
-                                <PopoverTrigger asChild>
-                                    <Volume2 size={20} />
-                                </PopoverTrigger>
-                                <PopoverContent
-                                    className="w-[150px] flex gap-1 items-center"
-                                    side="top"
-                                >
-                                    {volume === 0 ? (
-                                        <VolumeX size={20} onClick={mutedMusic} />
-                                    ) : (
-                                        <Volume2 size={20} onClick={mutedMusic} />
-                                    )}
-                                    <div className="slider relative w-full">
-                                        <Slider
-                                            defaultValue={[1]}
-                                            max={1}
-                                            value={[volume]}
-                                            min={0}
-                                            step={0.1}
-                                            onValueChange={handleVolumeChange}
-                                            className={cn('w-[100%]  py-2')}
-                                        />
-                                        <div
-                                            className="absolute flex items-center justify-center w-6 h-6 bg-card  text-xs"
-                                            style={{
-                                                left:
-                                                    volume === 1
-                                                        ? `calc(${volume * 100}% - 0.45rem - 10px)`
-                                                        : `calc(${volume * 100}% - 0.35rem - 4px)`,
-                                                top: '-2rem',
-                                                boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
-                                            }}
-                                        >
-                                            <p>{Math.round(volume * 100)}%</p>
+                        <div className="flex items-center">
+                            {isPlaying ? (
+                                <Button className="px-2" onClick={togglePlay} variant="transparent">
+                                    <Pause size={20} />
+                                </Button>
+                            ) : (
+                                <Button className="px-2" onClick={togglePlay} variant="transparent">
+                                    <Play size={18} />
+                                </Button>
+                            )}
+                            <div>
+                                <Popover>
+                                    <PopoverTrigger asChild>
+                                        <Button className="px-2" variant="transparent">
+                                            <Volume2 size={20} />
+                                        </Button>
+                                    </PopoverTrigger>
+                                    <PopoverContent
+                                        className="w-[100vw] -translate-y-2 md:w-[150px] py-0 pl-1 pr-2 flex gap-1 items-center"
+                                        side="top"
+                                        align="end"
+                                    >
+                                        {volume === 0 ? (
+                                            <Button
+                                                className="p-1"
+                                                onClick={mutedMusic}
+                                                variant="transparent"
+                                            >
+                                                <VolumeX size={22} />
+                                            </Button>
+                                        ) : (
+                                            <Button
+                                                className="p-1"
+                                                onClick={mutedMusic}
+                                                variant="transparent"
+                                            >
+                                                <Volume2 size={22} onClick={mutedMusic} />
+                                            </Button>
+                                        )}
+                                        <div className="relative w-full">
+                                            <Slider
+                                                defaultValue={[1]}
+                                                max={1}
+                                                value={[volume]}
+                                                min={0}
+                                                step={0.1}
+                                                onValueChange={handleVolumeChange}
+                                                className={cn('w-[100%] py-2')}
+                                            />
+                                            <Card
+                                                className="absolute flex -top-5 items-center justify-center shadow-xl px-1 text-xs"
+                                                style={{
+                                                    left:
+                                                        volume === 1
+                                                            ? `calc(${volume * 100}% - 0.45rem - 20px)`
+                                                            : `calc(${volume * 100}% - 0.35rem - 4px)`,
+                                                }}
+                                            >
+                                                <p>{Math.round(volume * 100)}%</p>
+                                            </Card>
                                         </div>
-                                    </div>
-                                </PopoverContent>
-                            </Popover>
+                                    </PopoverContent>
+                                </Popover>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <Slider
+                            defaultValue={[0]}
+                            value={[currentTime]}
+                            onValueChange={handleTimeChange}
+                            max={duration}
+                            step={1}
+                            className={cn('w-[100%] py-2')}
+                        />
+                        <div className="relative flex items-center w-[70px] h-6 grow-0 shrink-0 text-sm">
+                            <span className="absolute h-full left-0">
+                                {convertSecondsToTime(currentTime)}
+                            </span>
+                            <span className="absolute h-full top-0 left-1/2 -translate-x-1/2">
+                                /
+                            </span>
+                            <span className="absolute h-full top-0 right-0">
+                                {convertSecondsToTime(+duration.toFixed(0))}
+                            </span>
                         </div>
                     </div>
                 </div>
-                <div className="flex items-center gap-2">
-                    <Slider
-                        defaultValue={[0]}
-                        value={[currentTime]}
-                        onValueChange={handleTimeChange}
-                        max={duration}
-                        step={1}
-                        className={cn('w-[100%] py-2')}
-                    />
-                    <div className="flex items-center gap-1">
-                        <span>{convertSecondsToTime(currentTime)}</span> /{' '}
-                        <span>{convertSecondsToTime(+duration.toFixed(0))}</span>
-                    </div>
-                </div>
-            </Card>
-        </div>
+            </div>
+        </>
     )
 }
