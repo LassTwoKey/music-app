f756a8be3b08ac6e803b970fe296f9a7a6442d2d
LassTwoKey <dimas.super29@gmail.com>
2024-05-07 17:31:02 +0300

    wip: load files and chunks from db

--- backend/src/service/multer-config/multer-config.service.ts
+++ backend/src/service/multer-config/multer-config.service.ts
@@ -1,35 +1,36 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { MulterModuleOptions, MulterOptionsFactory } from '@nestjs/platform-express'
-import * as Grid from 'gridfs-stream'
-import mongoose from 'mongoose'
+import { MongoClient } from 'mongodb'
 import { GridFsStorage } from 'multer-gridfs-storage'
 
 const DB_NAME = process.env.DB_NAME
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
 
-const DB_URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.xbtinu8.mongodb.net/${DB_NAME}`
+const DB_URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.xbtinu8.mongodb.net`
 
 @Injectable()
 export class GridFsMulterConfigService implements MulterOptionsFactory {
     gridFsStorage: any
-    grf: any
+    db: any
+
     constructor() {
         this.gridFsStorage = new GridFsStorage({
             url: DB_URI,
             file: (req: any, file: any) => {
                 return new Promise((resolve) => {
                     const filename = 'filename' + Math.random().toString(16).slice(2)
-
                     const fileInfo = {
                         filename,
                     }
-
                     resolve(fileInfo)
                 })
             },
         })
-        this.grf = Grid(mongoose.createConnection(DB_URI), mongoose)
+    }
+
+    async onModuleInit() {
+        this.db = await MongoClient.connect(DB_URI, {}).then((client) => client.db(DB_NAME))
     }
 
     createMulterOptions(): MulterModuleOptions {
@@ -39,21 +40,29 @@ export class GridFsMulterConfigService implements MulterOptionsFactory {
     }
 
     async getFile(filename: string, res: any) {
-        // const readstream = this.grf.files.find({ filename }).sort({ uploadDate: 1 })
-        // return readstream
+        const file = await this.db.collection('fs.files').findOne({ filename: filename })
+        if (!file) {
+            throw new NotFoundException()
+        }
 
-        const readstream = this.grf.createReadStream({ filename: filename })
+        const downloadStream = this.db.collection('fs.chunks').find({ files_id: file._id })
 
-        // Устанавливаем HTTP-заголовок для указания типа содержимого файла
-        res.set('Content-Type', 'application/octet-stream')
+        res.set('Content-Type', file.contentType)
+        downloadStream.toArray((err: Error, chunks: any[]) => {
+            if (err) {
+                throw err
+            }
 
-        // Отправляем файл клиенту с помощью pipe
-        readstream.pipe(res)
+            if (chunks.length === 0) {
+                throw new NotFoundException()
+            }
 
-        // Обработка ошибок при чтении файла
-        readstream.on('error', (error: any) => {
-            console.error('Error reading file from GridFS:', error)
-            res.status(500).send('Internal Server Error')
+            const fileData = chunks.reduce(
+                (prev: Buffer[], current: any) => prev.concat(current.data.buffer),
+                [],
+            )
+            const fileBuffer = Buffer.concat(fileData)
+            res.send(fileBuffer)
         })
     }
 }
